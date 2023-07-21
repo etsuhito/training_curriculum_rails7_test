8002704e9b16098a18cce67b20ebd84d35ef4cd2
etsuhito <137661300+etsuhito@users.noreply.github.com>
2023-07-21 10:48:06 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -9,8 +9,8 @@ class CalendarsController < ApplicationController
   # 予定の保存
   def create
     binding.pry
-    Plan.create(plan_params)
-    redirect_to action: :index
+    Plan.create(content:plan_params[:content])
+    redirect_to "/plan"
   end
 
   private
@@ -35,7 +35,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
   end
